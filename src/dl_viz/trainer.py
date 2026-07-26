612b2a40d6226361e800c4554c0b2519abfe0900
Jack Settles <johntsettles@gmail.com>
2026-07-26 14:31:15 -0400

    temporarily adding the trainer file for now

---
+++ src/dl_viz/trainer.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Trainer:
+    def __init__():
+        pass
+
+    def _load_snapshot():
+        pass
+
+    def _save_snapshot():
+        pass
+
+    def _run_epoch():
+        pass
+
+    def _run_eval():
+        pass
