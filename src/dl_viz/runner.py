88df7732554f0ce86b185c610ddfd765c16cf83e
Jack Settles <johntsettles@gmail.com>
2026-07-29 13:55:09 -0400

    building runner

--- src/dl_viz/runner.py
+++ src/dl_viz/runner.py
@@ -1,29 +1,39 @@
 
 import argparse
+import pyyaml
 
 import torch.optim as optim
 
 from dl_viz.trainer import Trainer
 from dl_viz.data import DataConfig, get_cifar10_loaders
-import dl_viz.models
+import dl_viz.models as dvm
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument()
 
-def load_train_objects(args):
+def load_runner_dict(args):
+
+    model = dvm.MiniCNN()
+
     data_config = DataConfig()
     train_data, test_data = get_cifar10_loaders(data_config)
     
+
     return {
         "model": model,
         "train_data": train_data, 
         "val_data": None,
         "test_data": test_data,
+        "criterion": criterion,
+        "optimizer": optimizer,
+        "scheduler": scheduler,
+        "snapshot_path": snapshot_path,
+        "num_epochs": num_epochs
     }
 
 def main(args):
-    model, train_data, test_data = load_train_objects(args)
+    runner_dict = load_runner_dict(args)
     trainer = Trainer(
         model = model,
         train_data = train_data,
